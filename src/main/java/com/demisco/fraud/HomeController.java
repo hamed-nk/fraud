b3b7284f1a8bf6dd05a901852c25b6c60538fa55
i-laptop <hamed.dev70@gmail.com>
2022-02-13 15:54:10 +0330

    test1

---
+++ src/main/java/com/demisco/fraud/HomeController.java
@@ -0,0 +1,15 @@
+package com.demisco.fraud;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String getName(){
+        return "my name is hamed";
+    }
+}
